3e8399eefe15002b7f095b2950db532c8c99e835
Gae <ga3tano@live.it>
2026-07-05 19:20:39 +0200

    update servis uorcher

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ const name = 'Ciò che resta';
 
 // The version of the cache, changing this will force everything to be cached
 // again.
-const version = '0.1.21';
+const version = '0.1.22';
 
 const files = [
 
